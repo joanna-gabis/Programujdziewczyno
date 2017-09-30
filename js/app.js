7e39dde82e08deb9aaafc01497cad67a502ff91b
joanna-gabis <joanna.gabis@gmail.com>
2017-09-30 15:38:56 +0200

    excercise 1

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+var menu = (document.querySelector('.dropdown'));
+var forDropdown = (document.querySelector('.for-dropdown'));
+
+forDropdown.addEventListener('mouseover', function(){
+//console.log('hello');}
+
+menu.style.display = 'block';})
+
+forDropdown.addEventListener('mouseout', function(){
+//console.log('hello');}
+
+menu.style.display = 'none';})
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    });
---
+++ kopia/app.js
@@ -0,0 +1,25 @@
+console.log('działa!');
+
+var workshop= 'programuj dziewczyno!'
+console.log(workshop);
+
+var myName = 'Asia';
+var names = ['Olga', 'Asia', 'Ola', 'Magda'];
+
+console.log(names[1]);
+
+
+
+for (var i=0; i<=names.length-1; i++)
+{
+	console.log(names[i]);
+}
+
+var x = 4;
+var y = 2;
+
+if (x > y) {
+	console.log('hurra');
+} else{
+	console.log('łeeee');
+}
